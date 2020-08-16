f66be6ae02fa5d26dddebad9d2d5045668372c34
Gaurawa <kgaurawa@gmail.com>
2020-08-16 19:22:10 +0530

    First Evaluation

---
+++ app/src/main/java/com/example/g56_e2hgdeveloper/Room_Edit_delete.java
@@ -0,0 +1,14 @@
+package com.example.g56_e2hgdeveloper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Room_Edit_delete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room__edit_delete);
+    }
+}
